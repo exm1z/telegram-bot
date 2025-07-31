ce77a90eeed22f0bbbc422781a0311f993daf2b2
exm1z <daniel661234567890@gmail.com>
2025-07-31 03:15:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-BOT_TOKEN = os.environ["8264425647:AAHR8XQsep1kE5Oh4_ZQ4UKcwBndILXO8Hc"]
-ADMIN_CHAT_ID = int(os.environ["6902767271"])
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+ADMIN_CHAT_ID = int(os.environ["ADMIN_CHAT_ID"])
+
 
 
